cec03f50dd9e8e7c129f272d8198a7e47990b613
Ian Keough <ian@hypar.io>
2020-12-10 15:38:54 -0800

    Update the nuget package.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Installing the Hypar CLI..."
-dotnet tool install -g hypar.cli --version 0.8.1-alpha2
+dotnet tool install -g hypar.cli --version 0.8.1-alpha4
 export PATH="$PATH:/github/home/.dotnet/tools"
 echo "Hypar CLI installed."
 
